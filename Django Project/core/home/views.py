6e016d0fce17cefdffcb6fbd383a4df0640089f5
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-26 22:53:23 +0500

    Django table
    Django table

--- Django Project/core/home/views.py
+++ Django Project/core/home/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home/index.html')
+    peoples = [
+        {'name': 'ijaz', 'age': '29'},
+        {'name': 'Malak', 'age': '10'},
+        {'name': 'abbas', 'age': '24'},
+        {'name': 'waleed', 'age': '18'},
+    ]
+    return render(request, 'home/index.html', context={'peoples': peoples})
 
 
 def success_page(request):
