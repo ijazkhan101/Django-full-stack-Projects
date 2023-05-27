d2add62846cd85f0bbcc88c5cbfdccc19ff4dac7
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-27 19:14:45 +0500

    model
    model

--- Django Project/core/home/models.py
+++ Django Project/core/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    # id =models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(blank=True, null=True)
+    image = models.ImageField()
+    file = models.FileField()
