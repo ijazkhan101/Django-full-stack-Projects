c9eb95831a3c8a2147d3163171d4ca7f3f3b4a15
ijazdev101 <ijazkhan19932@gmail.com>
2023-06-17 13:23:40 +0500

    django project
    django project

--- Django Project/core/home/utils.py
+++
@@ -1,8 +0,0 @@
-from .models import Student
-import time
-
-
-def run_this_functions():
-    print(" function started ")
-    time.sleep(1)
-    print(" function Executed ")
--- README.md
+++
@@ -1,2 +0,0 @@
-### Django-full-stack-Projects
-Django full stack Projects
